8aa1a480a7cb8a5ef34ab10fa86d829580853e6d
AnastasiiaVV <vitchenkoanastasiia93@gmail.com>
2022-07-04 23:18:32 +0300

    Home_work_cours_asp_c#

---
+++ ASP/4/4/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using _4.Model;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4.Controllers
+{
+	public class HomeController : Controller
+	{
+		ReturnDataTime Model;
+		public IActionResult Index() {
+			return View();
+		}
+
+		public IActionResult Page1() {
+			return View(Model);
+		}
+
+		public IActionResult Page2() {
+			return View();
+		}
+
+		public HomeController() {
+			Model = new ReturnDataTime() {
+				ReturnData = DateTime.Now
+			};
+			
+
+		}
+		public IActionResult GetTime() {
+			return PartialView("_GetTime", Model);
+		}
+
+	}
+}
---
+++ ASP/4/4/Model/ReturnDataTime.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _4.Model
+{
+	public class ReturnDataTime
+	{
+		public DateTime ReturnData { get; set; }
+
+		//public ReturnDataTime(DateTime dateTime) {
+		//	ReturnData = dateTime;
+		//}
+	}
+}
